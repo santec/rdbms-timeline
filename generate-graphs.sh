cc7238aca0144688d3d5043cb3acd7704bd7e2b0
Federico Razzoli <f.razzoli@catawiki.nl>
2017-11-11 12:00:29 +0100

    add generate-graphs.sh

---
+++ generate-graphs.sh
@@ -0,0 +1,24 @@
+# check if graphviz is installed
+which dot > /dev/null
+if [ $? -ne 0 ];
+then
+    echo "Graphviz is required."
+    echo "Please download from http://www.graphviz.org/"
+    exit 1
+fi
+
+# generate PNG
+dot -Tpng -o RDBMS_timeline.png RDBMS_timeline.dot
+exit_code=$?
+if [ $exit_code -ne 0 ];
+then
+    echo "PNG generation failed with exit code $exit_code"
+fi
+
+# generate SVG
+dot -Tsvg -o RDBMS_timeline.svg RDBMS_timeline.dot
+exit_code=$?
+if [ $exit_code -ne 0 ];
+then
+    echo "SVG generation failed with exit code $exit_code"
+fi
